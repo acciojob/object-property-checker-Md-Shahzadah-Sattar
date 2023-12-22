fc5f0bb1bc83e9bd795c58e522d87acfaace032f
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2023-12-22 20:16:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-
 function hasKey(key) {
-  //   write your code here
+  // Check if the global object sampleObject contains the given key
+  return sampleObject.hasOwnProperty(key);
 }
 
+// Global object sampleObject
+const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+
+// Examples
+console.log(hasKey("red"));    // Output: true
+console.log(hasKey("blue"));   // Output: false
+console.log(hasKey("white"));  // Output: true
+
+
 // Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// const key = prompt("Enter Key.");
+// alert(hasKey(key));
